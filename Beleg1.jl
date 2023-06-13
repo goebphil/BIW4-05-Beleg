2a59b5c416d4dadffb784f31520596300b795865
goebphil <goeb.phil@hotmail.de>
2023-06-13 13:47:41 +0200

    Schleife und F-Fkt erneuert

--- Beleg1.jl
+++ Beleg1.jl
@@ -1,6 +1,7 @@
+
 using Plots
 
-#  Koeffizienten einer gebrochenrationalen Approximation der Steifigkeit
+# Koeffizienten einer gebrochenrationalen Approximation der Steifigkeit
 P0 = 10337706.5
 P1 = 48744.172
 P2 = 455.205728
@@ -14,44 +15,33 @@ Q2 = 2.71147593e-5
 Q3 = 5.42912842e-8
 Q4 = 6.94958499e-12
 
-###############################################################################
+# Abspaltung
+s1_0 = P5 / Q4
+s0_0 = (P4 - s1_0 * Q3) / Q4
+r3_0 = P3 - s0_0 * Q3 - s1_0 * Q2
+r2_0 = P2 - s0_0 * Q2 - s1_0 * Q1
+r1_0 = P1 - s0_0 * Q1 - s1_0
+r0_0 = P0 - s0_0
 
+s1_1 = Q4 / r3_0
+s0_1 = (Q3 - s1_1 * r2_0) / r3_0
+r2_1 = Q2 - s0_1 * r2_0 - s1_1 * r1_0
+r1_1 = Q1 - s0_1 * r1_0 - s1_1 * r0_0
+r0_1 = Q0 - s0_1 * r0_0
 
+s1_2 = r3_0 / r2_1
+s0_2 = (r2_0 - s1_2 * r1_1) / r2_1
+r1_2 = r1_0 - s0_2 * r1_1 - s1_2 * r0_1
+r0_2 = r0_0 - s0_2 * r0_1
 
-#Abspaltung
+s1_3 = r2_1 / r1_2
+s0_3 = (r1_1 - s1_3 * r0_2) / r1_2
+r0_3 = r0_1 - s0_3 * r0_2
 
-s1_0 = P5/Q4
-s0_0 = (P4-s1_0*Q3)/Q4
-r3_0 = P3-s0_0*Q3-s1_0*Q2
-r2_0 = P2-s0_0*Q2-s1_0*Q1
-r1_0 = P1-s0_0*Q1-s1_0
-r0_0 = P0-s0_0
-
-
-s1_1 = Q4/r3_0
-s0_1 = (Q3-s1_1*r2_0)/r3_0
-r2_1 = Q2-s0_1*r2_0-s1_1*r1_0
-r1_1 = Q1-s0_1*r1_0-s1_1*r0_0
-r0_1 = Q0-s0_1*r0_0
-
-
-s1_2 = r3_0/r2_1
-s0_2 = (r2_0-s1_2*r1_1)/r2_1
-r1_2 = r1_0-s0_2*r1_1-s1_2*r0_1
-r0_2 = r0_0-s0_2*r0_1
-
-
-s1_3 = r2_1/r1_2
-s0_3 = (r1_1-s1_3*r0_2)/r1_2
-r0_3 = r0_1-s0_3*r0_2
-
-
-s0_4 = r0_2/r0_3
-s1_4 = r1_2/r0_3
-
+s0_4 = r0_2 / r0_3
+s1_4 = r1_2 / r0_3
 
 f0 = 0
-
 
 A = [s1_0 0 0 0 0;
      0 -s1_1 0 0 0;
@@ -65,93 +55,50 @@ B = [s0_0 1 0 0 0;
      0 0 1 -s0_3 -1;
      0 0 0 -1 s0_4]
 
-
-
-r = [0;0;0;0;0]
+r = [0; 0; 0; 0; 0]
 
 println(A)
 println(B)
 println(r)
 
+# Zeitschrittlänge wird hier im Skript mit t anstatt delta t beschrieben
+delta_t = 0.00001 # s
+t = 0
+t_end = 1
+k = 1
 
-
-###############################################################################
-
-
-#Zeitschrittlänge wird hier im Skript mit t anstatt delta t beschrieben
-t = 0.0001 #s
-
-#Anzahl der Iteratiosschritte
-# Die Anzahl der Iterationsschritte ergeben sich aus der Größe von t
-
-
-j_ges = Int(1/t+1)
-j_Schritt = Int(1/t)
-println("Anzahl der Zeitschritte: ",j_Schritt)
-
-#Befüllen der Anfangsarrays
+# Array für die Darstellung des Plots
+z3 = Float64[]
 z0 = [0;0;0;0;0]
 
-#Array für die Darstellung des Plots
-z3 = zeros(j_ges)
-
-# Erstellen eines Arrays über die Zeitschritte, für in die Integration
-j = zeros(j_ges, 1)
-for i in 1:j_ges
-    j[i, 1] = (i - 1) * t
+function Belastung(t)
+    if t < 0 && t < 0.01
+        return t * [100000; 0; 0; 0; 0]
+    elseif t <= 0.01 && t < 0.02
+        return [100000; 0; 0; 0; 0]
+    elseif t <= 0.02 && t < 0.03
+        return -t * [100000; 0; 0; 0; 0]
+    else
+        return [0;0;0;0;0]
+    end
 end
 
+while true
+    z1 = inv(A + delta_t/2*B) * ((Belastung(t) + Belastung(t+delta_t))/2*delta_t - (delta_t/2*B-A)*z0)
 
 
-# Funktionen von F(t)
-# Diese sind in 3 Teilen unterteilt, wie aus der Grafik in der Aufgabenstellung zu entnehmen ist
-function F1(k)
-     return [1;0;0;0;0]*100000/0.01*k
+    global z0 = z1
+
+    push!(z3, z1[1])
+    global k += 1
+    global t += delta_t
+    if t >= t_end
+        break
+    end
 end
 
-F2 = [1;0;0;0;0]*100000
-
-function F3(k)
-     return [1;0;0;0;0]* -100000/0.01*k
-end
-
-
-# Integration über den Gesamten Zeitbereich von 1 Sekunden
-# Dabei wird mit der if Anweisung geschaut, in welchem Bereichen des Graphen man sich befindet
-for i =1:j_ges
-
-     if i == 1
-          z1 = (A+0.001/2*B)^-1 * (F1(j[i])+F1(j[i+1]))/2*t
-          global z0 = z1
-     end
-
-     if i>1 && i<=0.01*j_Schritt
-          z1 = inv((A+t/2*B)) * ((F1(j[i])+F1(j[i+1]))/2*t-(t/2*B-A)*z0)
-          global z0 = z1
-     end
-
-     if i>0.01*j_Schritt && i<=0.02*j_Schritt
-          z1 = inv((A+t/2*B)) * (F2*t-(t/2*B-A)*z0)
-          global z0 = z1
-     end
-
-     if i>0.02*j_Schritt && i<=0.03*j_Schritt
-          z1 = inv((A+t/2*B)) * ((F3(j[i])+F3(j[i+1]))/2*t-(t/2*B-A)*z0)
-          global z0 = z1
-     end
-
-     if i>0.03*j_Schritt
-          z1 = inv(A+t/2*B) * -(t/2*B-A)*z0
-          global z0 = z1
-     end
-
-     z3[i] = z1[1]
-
-end
-
-#Ausgabe der Ergebnisse
 println("Lösung für z1")
 println(z0)
 
-plot(j, z3, xlabel = "Sekunden", ylabel = "Werte für z1")
+plot(z3, xlabel="Sekunden", ylabel="Werte für z1")
 savefig("plot.png")
